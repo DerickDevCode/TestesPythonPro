d24b5ce7db9c218d9a7d6ecf9da15ac633a09f72
Dérick Eduardo <dreduardo2222@gmail.com>
2023-10-21 13:51:51 -0400

    Módulo Injeção de Dependências.

---
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -0,0 +1,18 @@
+from libpythonpro.spam.modelos import Usuario
+from libpythonpro.tests.test_spam.conftest import sessao
+
+
+def test_salvar_usuario(sessao):
+    usuario = Usuario(nome='Renzo', email='renzo@python.pro.br')
+    sessao.salvar(usuario)
+    assert isinstance(usuario.id, int)
+
+
+def test_listar_usuario(sessao):
+    usuarios = [
+        Usuario(nome='Renzo', email='renzo@python.pro.br'),
+        Usuario(nome='Luciano', email='renzo@python.pro.br')
+    ]
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    assert usuarios == sessao.listar()
